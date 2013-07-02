ec2115f7b910f3ed6d53f4bf8c4ccda837a5d407
Landy Wang <landy.wang@ef.com>
2013-07-02 12:56:35 +0800

    New a selenium test case: Login External.

---
+++ SeleniumTestCase/SeleniumTestCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeleniumTestCase
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            bool result = true;
+            TestClass testDemo = new TestClass();
+            testDemo.SetupTest();
+            testDemo.Steps();
+            result=testDemo.Verify();
+            testDemo.CleanUp();
+        }
+    }
+}
---
+++ SeleniumTestCase/SeleniumTestCase/test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumTestCase
+{
+    public class TestClass
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+       // private const string IE_DRIVER_PATH = @"D:\selenium-dotnet-2.33.0";
+
+
+        public void SetupTest()
+        {
+
+            var options = new InternetExplorerOptions();
+            options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+            //driver = new InternetExplorerDriver(IE_DRIVER_PATH, options);
+            driver = new InternetExplorerDriver(options);
+
+            baseURL = "http://qa.englishtown.com/partner/coolschool/Default.aspx";
+            verificationErrors = new StringBuilder();
+        }
+
+        public void Steps()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/partner/coolschool/Default.aspx");
+            driver.FindElement(By.Id("txtName")).Clear();
+            driver.FindElement(By.Id("txtName")).SendKeys("ct313");
+            driver.FindElement(By.Id("txtPassWord")).Clear();
+            driver.FindElement(By.Id("txtPassWord")).SendKeys("pass");
+            driver.FindElement(By.Id("loginTrigger")).Click();
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        public bool Verify()
+        {
+            if (driver.FindElement(By.Id("MyCourseViewNew")).Displayed)
+                return true;
+            return false;
+        }
+
+        public void CleanUp()
+        {
+            
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
